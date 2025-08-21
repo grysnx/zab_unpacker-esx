e01d7c4d82c630eec285fe13c975af9379b3c370
grysnx <65641533+grysnx@users.noreply.github.com>
2025-08-21 13:27:09 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'zab_unpacker'
 description 'Simple ox_inventory unpacker script'
 author 'VentuZAB'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -19,5 +19,8 @@ server_scripts {
     'server.lua'
 }
 
+dependencies {
+    'es_extended'
+}
+
 lua54 'yes'
-use_experimental_fxv2_oal 'yes'
